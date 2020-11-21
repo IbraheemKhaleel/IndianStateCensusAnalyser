ef185553b03fec733a697593f4d9c123a25d8ad2
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-21 10:47:15 +0530

    [Ibraheem Khaleel] Added: code for creating class which initialise main class object using fixture method

--- test_indian_state_census_analyser/conftest.py
+++ test_indian_state_census_analyser/conftest.py
@@ -5,5 +5,5 @@ from main_census_analyser.census_analyser import StateCensusAnalyser
 
 @pytest.fixture
 def instance_of_main_class():
-    state_census_analyser = StateCensusAnalyser()
+    state_census_analyser =  StateCensusAnalyser()
     return state_census_analyser
