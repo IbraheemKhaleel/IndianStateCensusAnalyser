eda8d4175840a9b8296858669794980897f3c9b0
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-21 10:59:27 +0530

    [Ibraheem Khaleel] Refactored : code for testing Indian state code entries, wrong file path and csv extensions

--- test_indian_state_census_analyser/test_census_analyser.py
+++ test_indian_state_census_analyser/test_census_analyser.py
@@ -3,8 +3,11 @@ import pytest
 from main_census_analyser.census_analyser_exception import WrongFilePathError, WrongExtensionCSVFile
 
 INDIA_STATE_CENSUS_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCensusData.csv'
-WRONG_CSV_PATH =  'C:/Users/Ibrahim Khaleel/PycharmProjects/test_indian_state_census_analyser/IndiaStateCensusData.csv'
+WRONG_CSV_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/test_indian_state_census_analyser/IndiaStateCensusData.csv'
 WRONG_FILE_EXTENSION = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCensusData.json'
+INDIA_STATE_CODE_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCode.csv'
+WRONG__STATE_CODE_CSV_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/IndiaStateCode.csv'
+WRONG_STATE_CODE_FILE_EXTENSION = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCode.json'
 
 
 def test_load_census_data(instance_of_main_class):
@@ -20,3 +23,18 @@ def test_wrong_filepath_when_analysed_should_raise_errors(instance_of_main_class
 def test_wrong_file_extension_when_analysed_should_raise_errors(instance_of_main_class):
     with pytest.raises(WrongExtensionCSVFile):
         instance_of_main_class.load_census_data(WRONG_FILE_EXTENSION)
+
+
+def test_load_state_code_data(instance_of_main_class):
+    count_of_entries = instance_of_main_class.load_census_data(INDIA_STATE_CODE_PATH)
+    assert count_of_entries == 37
+
+
+def test_wrong_state_code_filepath_when_analysed_should_raise_errors(instance_of_main_class):
+    with pytest.raises(WrongFilePathError):
+        instance_of_main_class.load_census_data(WRONG__STATE_CODE_CSV_PATH)
+
+
+def test_wrong_state_code_file_extension_when_analysed_should_raise_errors(instance_of_main_class):
+    with pytest.raises(WrongExtensionCSVFile):
+        instance_of_main_class.load_census_data(WRONG_STATE_CODE_FILE_EXTENSION)
