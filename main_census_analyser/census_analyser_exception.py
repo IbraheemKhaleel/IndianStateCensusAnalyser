ea8e466e7f78874e3a3a7df3e5844196fd7d3410
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-24 13:01:39 +0530

    [Ibraheem Khaleel] Refactored: code for testing methods which count entries, check exceptions and sorting the elements in the csv file

--- main_census_analyser/census_analyser_exception.py
+++ main_census_analyser/census_analyser_exception.py
@@ -1,11 +1,20 @@
-class CensusAnalyserException(Exception) :
-    def __init__(self, message):
-        super().__init__(message)
+import enum
 
 
-class WrongFilePathError(CensusAnalyserException):
-    pass
+class ExceptionType(enum.Enum):
+    """
+    Created an exception class with enums for different errors
+    """
+
+    WrongFilePathError = "Path of file is incorrect"
+    WrongExtensionCSVFile = "Extension of file is wrong"
+    WrongHeader = "Heading is corrupted"
+    WrongDelimiter = "Error occurred in delimiter matching"
 
 
-class WrongExtensionCSVFile(CensusAnalyserException):
-    pass
+class CensusAnalyserError(Exception):
+
+    def __init__(self, *args):
+
+        self.type = args[0]
+        self.message = args[1]
--- test_indian_state_census_analyser/test_census_analyser.py
+++ test_indian_state_census_analyser/test_census_analyser.py
@@ -1,13 +1,17 @@
 import pytest
 
-from main_census_analyser.census_analyser_exception import WrongFilePathError, WrongExtensionCSVFile
+from main_census_analyser.census_analyser_exception import CensusAnalyserError
 
-INDIA_STATE_CENSUS_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCensusData.csv'
-WRONG_CSV_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/test_indian_state_census_analyser/IndiaStateCensusData.csv'
-WRONG_FILE_EXTENSION = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCensusData.json'
-INDIA_STATE_CODE_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCode.csv'
-WRONG__STATE_CODE_CSV_PATH = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/IndiaStateCode.csv'
-WRONG_STATE_CODE_FILE_EXTENSION = 'C:/Users/Ibrahim Khaleel/PycharmProjects/IndianStateCensusAnalyser/test_indian_state_census_analyser/IndiaStateCode.json'
+INDIA_STATE_CENSUS_PATH = '../test_indian_state_census_analyser/IndiaStateCensusData.csv'
+WRONG_CSV_PATH = '../wrong_path/test_indian_state_census_analyser/IndiaStateCensusData.csv'
+WRONG_FILE_EXTENSION = '../test_indian_state_census_analyser/IndiaStateCensusData.json'
+WRONG_DELIMITER_CENSUS_PATH = '../test_indian_state_census_analyser/DelimiterIndiaCensusCode.csv'
+MISSING_HEADER_CENSUS_PATH = '../test_indian_state_census_analyser/MissingHeaderCensusCode.csv'
+INDIA_STATE_CODE_PATH = '../test_indian_state_census_analyser/IndiaStateCode.csv'
+WRONG__STATE_CODE_CSV_PATH = '../IndianStateCensusAnalyser/IndiaStateCode.csv'
+WRONG_STATE_CODE_FILE_EXTENSION = '../test_indian_state_census_analyser/IndiaStateCode.json'
+MISSING_HEADER_STATE_CODE_PATH = '../test_indian_state_census_analyser/MissingHeaderIndiaStateCode.csv'
+WRONG_DELIMITER_STATE_CODE_PATH = '../test_indian_state_census_analyser/DelimiterIndiaStateCode.csv'
 
 
 def test_load_census_data(instance_of_main_class):
@@ -15,14 +19,14 @@ def test_load_census_data(instance_of_main_class):
     assert count_of_entries == 29
 
 
-def test_wrong_filepath_when_analysed_should_raise_errors(instance_of_main_class):
-    with pytest.raises(WrongFilePathError):
-        instance_of_main_class.load_census_data(WRONG_CSV_PATH)
-
-
-def test_wrong_file_extension_when_analysed_should_raise_errors(instance_of_main_class):
-    with pytest.raises(WrongExtensionCSVFile):
-        instance_of_main_class.load_census_data(WRONG_FILE_EXTENSION)
+@pytest.mark.parametrize("path_of_file, expected",
+                         [(WRONG_CSV_PATH, CensusAnalyserError),
+                          (WRONG_FILE_EXTENSION, CensusAnalyserError),
+                          (WRONG_DELIMITER_CENSUS_PATH, CensusAnalyserError),
+                          (MISSING_HEADER_CENSUS_PATH, CensusAnalyserError)])
+def test_wrong_filepath_when_analysed_should_raise_errors(path_of_file, expected, instance_of_main_class):
+    with pytest.raises(expected):
+        instance_of_main_class.load_census_data(path_of_file)
 
 
 def test_load_state_code_data(instance_of_main_class):
@@ -30,11 +34,31 @@ def test_load_state_code_data(instance_of_main_class):
     assert count_of_entries == 37
 
 
-def test_wrong_state_code_filepath_when_analysed_should_raise_errors(instance_of_main_class):
-    with pytest.raises(WrongFilePathError):
-        instance_of_main_class.load_census_data(WRONG__STATE_CODE_CSV_PATH)
+@pytest.mark.parametrize("path_of_file, expected", [(WRONG__STATE_CODE_CSV_PATH, CensusAnalyserError),
+                                                    (WRONG_STATE_CODE_FILE_EXTENSION, CensusAnalyserError),
+                                                    (MISSING_HEADER_STATE_CODE_PATH, CensusAnalyserError),
+                                                    (WRONG_DELIMITER_STATE_CODE_PATH, CensusAnalyserError)
+                                                    ])
+def test_wrong_state_code_filepath_when_analysed_should_raise_errors(path_of_file, expected, instance_of_main_class):
+    with pytest.raises(expected):
+        instance_of_main_class.load_census_data(path_of_file)
 
 
-def test_wrong_state_code_file_extension_when_analysed_should_raise_errors(instance_of_main_class):
-    with pytest.raises(WrongExtensionCSVFile):
-        instance_of_main_class.load_census_data(WRONG_STATE_CODE_FILE_EXTENSION)
+def test_sorting_datas_by_state(instance_of_main_class):
+    state_name = instance_of_main_class.sorting_datas_by_state(INDIA_STATE_CENSUS_PATH)
+    assert state_name[0] == "Andhra Pradesh"
+
+
+def test_sorting_datas_by_last_state(instance_of_main_class):
+    state_name = instance_of_main_class.sorting_datas_by_state(INDIA_STATE_CENSUS_PATH)
+    assert state_name[1] == "West Bengal"
+
+
+def test_sorting_datas_by_first_state_code(instance_of_main_class):
+    state_code = instance_of_main_class.sorting_datas_by_state_code(INDIA_STATE_CODE_PATH)
+    assert state_code[0] == "AD"
+
+def test_sorting_datas_by_last_state_code(instance_of_main_class):
+    state_code = instance_of_main_class.sorting_datas_by_state_code(INDIA_STATE_CODE_PATH)
+    assert state_code[1] == "WB"
+
