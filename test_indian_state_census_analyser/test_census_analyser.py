70dd0bc6b5b5e20537e929fe8f79d928a95f531c
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-25 08:38:03 +0530

    [Ibraheem Khaleel] Refactored: code for testing methods which sorts state, area , population, state code and population density.

--- test_indian_state_census_analyser/test_census_analyser.py
+++ test_indian_state_census_analyser/test_census_analyser.py
@@ -46,19 +46,34 @@ def test_wrong_state_code_filepath_when_analysed_should_raise_errors(path_of_fil
 
 def test_sorting_datas_by_state(instance_of_main_class):
     state_name = instance_of_main_class.sorting_datas_by_state(INDIA_STATE_CENSUS_PATH)
-    assert state_name[0] == "Andhra Pradesh"
+    assert state_name[0]['State'] == "Andhra Pradesh"
 
 
 def test_sorting_datas_by_last_state(instance_of_main_class):
     state_name = instance_of_main_class.sorting_datas_by_state(INDIA_STATE_CENSUS_PATH)
-    assert state_name[1] == "West Bengal"
+    assert state_name[len(state_name) - 1]['State'] == "West Bengal"
 
 
 def test_sorting_datas_by_first_state_code(instance_of_main_class):
     state_code = instance_of_main_class.sorting_datas_by_state_code(INDIA_STATE_CODE_PATH)
-    assert state_code[0] == "AD"
+    assert state_code[0]['StateCode'] == "AD"
+
 
 def test_sorting_datas_by_last_state_code(instance_of_main_class):
     state_code = instance_of_main_class.sorting_datas_by_state_code(INDIA_STATE_CODE_PATH)
-    assert state_code[1] == "WB"
+    assert state_code[len(state_code) - 1]['StateCode'] == "WB"
 
+
+def test_sort_by_largest_area(instance_of_main_class):
+    largest_area = instance_of_main_class.sort_by_area(INDIA_STATE_CENSUS_PATH)
+    assert largest_area[0]['AreaInSqKm'] == "342239"
+
+
+def test_sort_by_population_density(instance_of_main_class):
+    largest_population_density = instance_of_main_class.sort_by_population_density(INDIA_STATE_CENSUS_PATH)
+    assert largest_population_density[0].get('DensityPerSqKm') == "1102"
+
+
+def test_sort_by_population(instance_of_main_class):
+    largest_population = instance_of_main_class.sort_by_population(INDIA_STATE_CENSUS_PATH)
+    assert largest_population[0].get('Population') == "199812341"
